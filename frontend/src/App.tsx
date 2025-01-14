35a6d84fdccb912310281a1b840c02d57a0dc9bf
laraduerlich <lara.duerlich@t-online.com>
2025-01-14 10:28:25 +0100

    add DashboardPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,16 @@
 import './App.css'
+import {Route, Routes} from "react-router-dom";
+import DashboardPage from "./page/DashboardPage.tsx"
+import NavBar from "./compontent/NavBar.tsx";
 
 function App() {
 
   return (
     <>
+      <NavBar/>
+      <Routes>
+        <Route path={"/"} element={<DashboardPage />} />
+      </Routes>
 
     </>
   )
---
+++ frontend/src/page/DashboardPage.tsx
@@ -0,0 +1,9 @@
+
+export default function DashboardPage() {
+
+    return(
+        <>
+            <h1>Meine ToDo's</h1>
+        </>
+    )
+}
